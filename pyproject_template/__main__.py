b16ef432fa8bae89f2597e258607d0cb3d3ff656
EdmundGoodman <egoodman3141@gmail.com>
2023-07-12 12:39:31 +0100

    Add simple source code template

---
+++ pyproject_template/__main__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""A python template project."""
+
+from pyproject_template.main import main
+
+if __name__ == "__main__":
+    main()
---
+++ pyproject_template/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Docstring for an example tool."""
+
+
+def main() -> None:
+    """Say hello."""
+    print("Hello world")
