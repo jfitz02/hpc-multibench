4f75cadd05df6c40b3d389a38d19eded0f7f7536
EdmundGoodman <egoodman3141@gmail.com>
2024-02-12 23:56:03 +0000

    Add more full YAML examples

--- src/hpc_multibench/main.py
+++ src/hpc_multibench/main.py
@@ -9,6 +9,6 @@ from hpc_multibench.yaml_ingest import get_benches
 
 def main() -> None:  # pragma: no cover
     """Run the tool."""
-    yaml_path = Path("./testing_plan.yaml")
+    yaml_path = Path("./yaml_examples/cpp_rust_comp_plan.yaml")
     benches = get_benches(yaml_path)
     print(benches)
