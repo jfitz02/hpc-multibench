cb728b917d149eb39dc1153b40f7db8267e4980b
EdmundGoodman <egoodman3141@gmail.com>
2024-02-15 01:38:36 +0000

    Add enum for different operating modes

--- src/hpc_multibench/main.py
+++ src/hpc_multibench/main.py
@@ -2,30 +2,33 @@
 # -*- coding: utf-8 -*-
 """Docstring for an example tool."""
 
+from enum import Enum, auto
 from pathlib import Path
 
 from hpc_multibench.analysis import analyse
 from hpc_multibench.configuration import DEFAULT_OUTPUT_DIRECTORY, wait_till_queue_empty
 from hpc_multibench.yaml_ingest import get_benches
 
-DRY_RUN: bool = True
+class Mode(Enum):
+    RUN = auto()
+    ANALYSE = auto()
+    ALL = auto()
 
 
-def main(yaml_path: Path) -> None:  # pragma: no cover
+def main(yaml_path: Path, mode: Mode = Mode.ANALYSE) -> None:  # pragma: no cover
     """Run the tool."""
-    # Build and run the test benches from the YAML
-    for bench in get_benches(yaml_path).values():
-        for run in bench:
-            if DRY_RUN:
-                print(run)
-            else:
+    benches = get_benches(yaml_path)
+    
+    if mode in (Mode.RUN, Mode.ALL):
+        # Build and run the test benches from the YAML
+        for bench in benches.values():
+            for run in bench:
                 run.run()
 
-    # Coudl do different modes? run/analyse/all
-
-    # Wait till the test benches are don
-    if not DRY_RUN:
+        # Wait till the test benches are done
         wait_till_queue_empty()
 
-    # Perform analysis on the results of the run
-    analyse(DEFAULT_OUTPUT_DIRECTORY)
+    if mode in (Mode.ANALYSE, Mode.ALL):
+        # Perform analysis on the results of the run
+        for bench_name in benches.keys():
+            analyse(DEFAULT_OUTPUT_DIRECTORY / bench_name)
