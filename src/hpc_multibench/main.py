08edaf96487702d5129a84329a48a93a22d43877
EdmundGoodman <egoodman3141@gmail.com>
2024-03-06 03:10:04 +0000

    Restructure parser for interactive mode

--- src/hpc_multibench/main.py
+++ src/hpc_multibench/main.py
@@ -11,6 +11,8 @@ from hpc_multibench.test_plan import TestPlan
 def get_parser() -> ArgumentParser:  # pragma: no cover
     """Get the argument parser for the tool."""
     parser = ArgumentParser(description="A tool to spawn and analyse HPC jobs.")
+    # As an argument of the base tool not subcommands, due to the ergonomics of
+    # running `record` then `report` on same yaml file in sequence
     parser.add_argument(
         "-y",
         "--yaml-path",
@@ -18,12 +20,6 @@ def get_parser() -> ArgumentParser:  # pragma: no cover
         type=Path,
         help="the path to the configuration YAML file",
     )
-    # # Consider ergonomics of `subcommand yaml_path`, when running `record`
-    # # then `report` on same yaml file in sequence
-    # for sub_parser in (parser_record, parser_report, parser_interactive):
-    #     sub_parser.add_argument(
-    #         "yaml_path", type=Path, help="the path to the configuration YAML file"
-    #     )
 
     sub_parsers = parser.add_subparsers(dest="command", required=True)
     parser_record = sub_parsers.add_parser(
@@ -47,11 +43,14 @@ def get_parser() -> ArgumentParser:  # pragma: no cover
         action="store_true",
         help="don't delete any previous run results of the test benches",
     )
-    _parser_report = sub_parsers.add_parser(
+    parser_report = sub_parsers.add_parser(
         "report", help="report analysis about completed test bench runs"
     )
-    _parser_interactive = sub_parsers.add_parser(
-        "interactive", help="launch interactive mode"
+    parser_report.add_argument(
+        "-i",
+        "--interactive",
+        action="store_true",
+        help="show the interactive TUI",
     )
     return parser
 
@@ -65,7 +64,7 @@ def main() -> None:  # pragma: no cover
         test_plan.record_all(args)
 
     elif args.command == "report":
-        test_plan.report_all(args)
-
-    elif args.command == "interactive":
-        raise NotImplementedError("Interactive mode is not implemented!")
+        if args.interactive:
+            raise NotImplementedError("Interactive mode is not implemented!")
+        else:
+            test_plan.report_all(args)
